7d89d3e526cec44adce39bc710225bd0a62ab3a8
Randi Nash <randidena@live.com>
2017-09-19 00:42:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+def fizzbuzz(hollaback)
+  if hollaback % 3 == 0 && hollaback % 5 == 0
     "FizzBuzz"
-  elsif int % 3 == 0
+  elsif hollaback % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif hollaback % 5 == 0
     "Buzz"
   end
 end
